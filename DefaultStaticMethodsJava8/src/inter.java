10a94a99ca30ff58b340c418bd0f03120f6f4ccf
Naresh2300 <126353278+Naresh2300@users.noreply.github.com>
2023-11-23 23:26:41 +0530

    Add files via upload

---
+++ DefaultStaticMethodsJava8/src/inter.java
@@ -0,0 +1,14 @@
+
+public interface inter {
+	
+	public static void main(String args[]) {
+		System.out.println("main Method inside Inteface");
+	}
+
+}
+
+
+// From java 1.8 version we can declare Main method inside Inteface
+// and we overriding is not possible in static interface because a class cannot
+//  implement Static Inteface method
+// we can define same method name as defined in interface but cannot override It.
