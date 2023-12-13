a6a7e40e36c41d22af782ba2b989a533392937fc
Naresh2300 <126353278+Naresh2300@users.noreply.github.com>
2023-12-13 23:54:39 +0530

    Add files via upload

---
+++ stringpractice/Spaces.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+public class Spaces {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String s1=" Hi j a v a is G o o d";
+		
+		String str2=s1.replaceAll("\\s", "");
+		
+		System.out.println("The WhiteSpaces are Removed: "+str2);
+
+	}
+
+}
