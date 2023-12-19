bb8953dcca8a2bf33878be21ddc7e28450ae2a55
Naresh2300 <126353278+Naresh2300@users.noreply.github.com>
2023-12-19 22:11:31 +0530

    Add files via upload

---
+++ Date&TImeAPI/src/practice/DateTime.java
@@ -0,0 +1,19 @@
+package practice;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DateTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LocalDate date=LocalDate.now();
+		
+		LocalTime time=LocalTime.now();
+		System.out.println(date);
+		System.out.println(time);
+
+	}
+
+}
